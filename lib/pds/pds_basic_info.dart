c49d59d067c625d0efde7653624fb7b3ae982e8c
Mark Angelo Bon <mdbon@dilg.gov.ph>
2022-11-04 15:53:39 +0800

    api data conts

--- lib/pds/pds_basic_info.dart
+++ lib/pds/pds_basic_info.dart
@@ -13,8 +13,8 @@ class PdsBasicInfo extends StatefulWidget {
 }
 
 Future<Album> fetchAlbum() async {
-  final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  final response = await http.get(Uri.parse(
+      'https://hris-admin.dilg.gov.ph/dropdown/getuserinfo/load-employee'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
